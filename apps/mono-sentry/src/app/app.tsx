2d3a692e4d1adcc82805fc907a92512e69f1b89b
yuexing09021 <yuexing09021@gmail.com>
2022-01-07 10:43:53 +0800

    initial commit

---
+++ apps/mono-sentry/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.less';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="mono-sentry" />
+      <div />
+    </>
+  );
+}
+
+export default App;
