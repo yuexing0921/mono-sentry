8696e404ce6b0c3c7c6acc6ebf8ad296856abbff
yuexing09021 <yuexing09021@gmail.com>
2022-01-12 12:00:07 +0800

    feat: custom tag

--- libs/common/src/lib/sentry.ts
+++ libs/common/src/lib/sentry.ts
@@ -23,12 +23,24 @@ export const Sentry = {
         `[Sentry] The name of projectName was transmitted incorrectly, and ${projectName} was not found`
       );
     }
+
     _Sentry.onLoad(() => {
       console.log(`Sentry release`, buildInfo[projectName].release);
       console.log('Sentry load succeeded');
       isLoadSentry = true;
     });
+    _Sentry.setTag('project', projectName);
     _Sentry.init({
+      beforeSend(event) {
+        console.log(event);
+        const { exception = {} } = event;
+        if (Array.isArray(exception.values) && exception.values[0]) {
+          const { value = '' } = exception.values[0];
+          if (value.indexOf('NetworkError') > -1) return null;
+          if (value.indexOf('Failed to fetch') > -1) return null;
+        }
+        return event;
+      },
       dsn: 'https://1c9f0a8bbd484e44aff738ffe078fb1e@o1101484.ingest.sentry.io/6128707',
       // 如果并发量很大，这个数据可以设置的小一点
       tracesSampleRate: 1.0,
